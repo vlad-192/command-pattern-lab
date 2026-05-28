cc24431320e4bb01b7f7aa4ae9aaea12a8dfcdeb
vlad-192 <vladlotnik825@gmail.com>
2026-05-28 10:24:34 +0300

    fghjkl;

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/vlad-192/workspace/projects/command-pattern-lab
-# Build directory: /home/vboxuser/vlad-192/workspace/projects/command-pattern-lab/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(command_test "/home/vboxuser/vlad-192/workspace/projects/command-pattern-lab/build/command_test")
-set_tests_properties(command_test PROPERTIES  _BACKTRACE_TRIPLES "/home/vboxuser/vlad-192/workspace/projects/command-pattern-lab/CMakeLists.txt;24;add_test;/home/vboxuser/vlad-192/workspace/projects/command-pattern-lab/CMakeLists.txt;0;")
